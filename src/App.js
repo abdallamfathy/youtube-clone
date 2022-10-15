7f87db39d4b4b671e2c520ff354aa96d2b71432f
abdallamfathy <abdallamfathy@gmail.com>
2022-10-15 13:06:51 +0200

    Add utils

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { Box } from '@mui/material';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
+import {Navbar , Feed , SearchFeed , VideoDetail , ChannelDetail} from "./components"
 const App = () => (
   <BrowserRouter>
-    <Box>
+    <Box sx={{backgroundColor: "#000"}}>
+      <Navbar/>
       <Routes>
         <Route path='/' exact element={<Feed/>}/>
-        <Route path='/video/:id' exact element={<VideoDetail/>}/>
-        <Route path='/channel/:id' exact element={<ChannelDetail/>}/>
-        <Route path='/search/:searchTerm' exact element={<SearchFeed/>}/>
+        <Route path='/video/:id'  element={<VideoDetail/>}/>
+        <Route path='/channel/:id'  element={<ChannelDetail/>}/>
+        <Route path='/search/:searchTerm'  element={<SearchFeed/>}/>
       </Routes>
     </Box>
   </BrowserRouter>
