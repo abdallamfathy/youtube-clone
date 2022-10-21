f11d0cc7d03488026e098a417491d1bb34d12775
abdallamfathy <abdallamfathy@gmail.com>
2022-10-21 16:34:55 +0200

    some edits

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,33 +1,46 @@
-import { CheckCircle } from '@mui/icons-material'
-import { CardContent, CardMedia, Typography } from '@mui/material'
-import { Box } from '@mui/system'
-import React, { useEffect } from 'react'
-import { Link, useParams } from 'react-router-dom'
-import { demoProfilePicture } from '../utils/constants'
-import { fetchFromApi } from '../utils/fetchFromApi'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import { Videos, ChannelCard } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const ChannelDetail = () => {
+  const [channelDetail, setChannelDetail] = useState();
+  const [videos, setVideos] = useState(null);
 
-  const { id } = useParams()
-  const [channelDetail , setChannelDetail] = (null)
-  const [videos , setVideos] = ([])
+  const { id } = useParams();
 
+  useEffect(() => {
+    const fetchResults = async () => {
+      const data = await fetchFromAPI(`channels?part=snippet&id=${id}`);
 
-  // console.log(channelDetail);
+      setChannelDetail(data?.items[0]);
 
-  useEffect(()=>{
-    fetchFromApi(`channels?part=snippet&id=${id}`)
-    .then((data) => setChannelDetail(data?.items[0]))
-    
-    fetchFromApi(`search?channelId=${id}&part=snippet&order=date`)
-    .then((data) => setVideos(data?.items))
-  },[id])
+      const videosData = await fetchFromAPI(`search?channelId=${id}&part=snippet%2Cid&order=date`);
+
+      setVideos(videosData?.items);
+    };
+
+    fetchResults();
+  }, [id]);
 
   return (
-    <Box>
-      {id}
+    <Box minHeight="95vh">
+      <Box>
+        <div style={{
+          height:'300px',
+          background: 'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)',
+          zIndex: 10,
+        }} />
+        <ChannelCard channelDetail={channelDetail} marginTop="-93px" />
+      </Box>
+      <Box p={2} display="flex">
+      <Box sx={{ mr: { sm: '100px' } }}/>
+        <Videos videos={videos} />
+      </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default ChannelDetail
+export default ChannelDetail;
--- src/utils/fetchFromApi.js
+++ src/utils/fetchFromApi.js
@@ -16,7 +16,7 @@ const options = {
 
 
 
-export const fetchFromApi = async(url) => {
+export const fetchFromAPI = async(url) => {
     const {data} =  await axios.get(`${BASE_URL}/${url}`,options);
 
     return data;
